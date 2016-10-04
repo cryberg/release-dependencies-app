e2b3765b592e36df30f951ec7d3c406b8d2a3edf
Claire <csr6ad@virginia.edu>
2016-10-04 18:58:26 +0000

    Standardize syntax

--- App.js
+++ App.js
@@ -82,7 +82,7 @@ Ext.define('CustomApp', {
             var iterationFilter = {
                 property: 'Iteration.Name',
                 operator: '=',
-                value: this.down('#iterationComboBox').getRawValue().split(" (")[0]
+                value: this.down('#iterationComboBox').getRawValue().split(' (')[0]
             };
         }
         
@@ -90,7 +90,7 @@ Ext.define('CustomApp', {
             var releaseFilter = {
                 property: 'Release.Name',
                 operator: '=',
-                value: this.down('#stateComboBox').getRawValue().split(" (")[0]
+                value: this.down('#stateComboBox').getRawValue().split(' (')[0]
             };
         }
         
@@ -121,17 +121,17 @@ Ext.define('CustomApp', {
             autoLoad: true,
             remoteSort: false,
             fetch:[
-        	    "FormattedID", 
-            	"Name",
-            	"Project",
-            	"Release",
-            	"Feature",
-            	"Milestones",
-            	"ScheduleState",
-            	"Successors",
-            	"Iteration",
-            	"DueDate",
-            	"Predecessors"
+        	    'FormattedID', 
+            	'Name',
+            	'Project',
+            	'Release',
+            	'Feature',
+            	'Milestones',
+            	'ScheduleState',
+            	'Successors',
+            	'Iteration',
+            	'DueDate',
+            	'Predecessors'
         	],
         	filters:  Rally.data.QueryFilter.or([{
                 property: 'Predecessors.ObjectID',
@@ -161,31 +161,31 @@ Ext.define('CustomApp', {
             self = this;
         _.each(data, function(story) {
             var s = { 
-            	Feature: story.get("Feature"), 
-            	FormattedID: story.get("FormattedID"), 
-            	StoryNumericID: Number(story.get("FormattedID").replace(/\D+/g, '')),
-            	Name: story.get("Name"), 
-            	Project: (story.get("Project") ? story.get("Project").Name : ""),
-            	Release: (story.get("Release") ? story.get("Release").Name : ""),
-            	_ref: story.get("_ref"), 
+            	Feature: story.get('Feature'), 
+            	FormattedID: story.get('FormattedID'), 
+            	StoryNumericID: Number(story.get('FormattedID').replace(/\D+/g, '')),
+            	Name: story.get('Name'), 
+            	Project: (story.get('Project') ? story.get('Project').Name : ''),
+            	Release: (story.get('Release') ? story.get('Release').Name : ''),
+            	_ref: story.get('_ref'), 
             	Predecessor: {},
-            	PredName: "",
-            	PredNumericID: "",
-            	PredProject: "",
-            	PredScheduleState: "",
-            	PredIteration: "",
+            	PredName: '',
+            	PredNumericID: '',
+            	PredProject: '',
+            	PredScheduleState: '',
+            	PredIteration: '',
             	PredIterationSortNum: 0,
             	PredDueDate: null,
             	Successor: {},
-            	SuccName: "",
-            	SuccNumericID: "",
-            	SuccProject: "",
-            	SuccScheduleState: "",
-            	SuccIteration: "",
+            	SuccName: '',
+            	SuccNumericID: '',
+            	SuccProject: '',
+            	SuccScheduleState: '',
+            	SuccIteration: '',
             	SuccIterationSortNum: 0,
-            	StoryScheduleState: story.get("ScheduleState"),
-            	Iteration: (story.get("Iteration") ? story.get("Iteration").Name : ""),
-                DueDate: story.get("DueDate")
+            	StoryScheduleState: story.get('ScheduleState'),
+            	Iteration: (story.get('Iteration') ? story.get('Iteration').Name : ''),
+                DueDate: story.get('DueDate')
             };
             
             if (s.Feature) {
@@ -197,14 +197,14 @@ Ext.define('CustomApp', {
             
             s.IterationSortNumber = this._getSortableIteration(s.Iteration);
             
-            var predecessorsStore = story.getCollection("Predecessors");
+            var predecessorsStore = story.getCollection('Predecessors');
             var predecessorPromise = predecessorsStore.load({
-                fetch: ["FormattedID", "Name", "Project", "ScheduleState", "Iteration", "DueDate"]
+                fetch: ['FormattedID', 'Name', 'Project', 'ScheduleState', 'Iteration', 'DueDate']
             });
 
-            var successorsStore = story.getCollection("Successors");
+            var successorsStore = story.getCollection('Successors');
             var successorPromise = successorsStore.load({
-                fetch: ["FormattedID", "Name", "Project", "ScheduleState", "Iteration"]
+                fetch: ['FormattedID', 'Name', 'Project', 'ScheduleState', 'Iteration']
             });
             
             promises.push(predecessorPromise, successorPromise);
@@ -224,41 +224,41 @@ Ext.define('CustomApp', {
                     
                     var predecessorsArray = story.PredecessorsStore.getRange();
                     _.each(predecessorsArray, function(predecessor) {
-                        var s = JSON.parse(JSON.stringify(stories.get(story.get("FormattedID"))));
-                        var iteration = (predecessor.get("Iteration") ? predecessor.get("Iteration").Name : "");
+                        var s = JSON.parse(JSON.stringify(stories.get(story.get('FormattedID'))));
+                        var iteration = (predecessor.get('Iteration') ? predecessor.get('Iteration').Name : '');
                         s.Predecessor = {
-                            _ref: predecessor.get("_ref"), 
-                			FormattedID: predecessor.get("FormattedID")
+                            _ref: predecessor.get('_ref'), 
+                			FormattedID: predecessor.get('FormattedID')
                         };
-                        s.PredName = predecessor.get("Name");
+                        s.PredName = predecessor.get('Name');
                         s.PredNumericID = Number(s.Predecessor.FormattedID.replace(/\D+/g, ''));
-                        s.PredProject = (predecessor.get("Project") ? predecessor.get("Project").Name : "");
-                        s.PredScheduleState = predecessor.get("ScheduleState");
+                        s.PredProject = (predecessor.get('Project') ? predecessor.get('Project').Name : '');
+                        s.PredScheduleState = predecessor.get('ScheduleState');
                         s.PredIteration = iteration;
                         s.PredIterationSortNum = self._getSortableIteration(iteration);
-                        s.PredDueDate = predecessor.get("DueDate");
+                        s.PredDueDate = predecessor.get('DueDate');
                         storyDuplicationArray.push(s);
                     });
                     
                     var successorsArray = story.SuccessorsStore.getRange();
                     _.each(successorsArray, function(successor) {
-                        var s = JSON.parse(JSON.stringify(stories.get(story.get("FormattedID"))));
-                        var iteration = (successor.get("Iteration") ? successor.get("Iteration").Name : "");
+                        var s = JSON.parse(JSON.stringify(stories.get(story.get('FormattedID'))));
+                        var iteration = (successor.get('Iteration') ? successor.get('Iteration').Name : '');
                         s.Successor = { 
-	            			_ref: successor.get("_ref"), 
-	            			FormattedID: successor.get("FormattedID")
+	            			_ref: successor.get('_ref'), 
+	            			FormattedID: successor.get('FormattedID')
 	            		};
-	            		s.SuccName = successor.get("Name");
+	            		s.SuccName = successor.get('Name');
 	            		s.SuccNumericID = Number(s.Successor.FormattedID.replace(/\D+/g, ''));
-	            		s.SuccProject = (successor.get("Project") ? successor.get("Project").Name : "");
-	            		s.SuccScheduleState = successor.get("ScheduleState");
+	            		s.SuccProject = (successor.get('Project') ? successor.get('Project').Name : '');
+	            		s.SuccScheduleState = successor.get('ScheduleState');
 	            		s.SuccIteration = iteration;
 	            		s.SuccIterationSortNum = self._getSortableIteration(iteration);
                         storyDuplicationArray.push(s);
                     });
              
                     if(storyDuplicationArray.length !== 0) {
-                        stories.set(story.get("FormattedID"), storyDuplicationArray);
+                        stories.set(story.get('FormattedID'), storyDuplicationArray);
                     }
                     
                 });
@@ -272,9 +272,9 @@ Ext.define('CustomApp', {
     
     _getSortableIteration: function(_iterationStr) {
         if(!!_iterationStr) {
-            var iterationStr = _iterationStr.split(" ")[1];
+            var iterationStr = _iterationStr.split(' ')[1];
             var iterationDecimal = iterationStr.split('.')[1].charCodeAt(0) - 64;
-            iterationStr = iterationStr.split('.')[0] + "." + iterationDecimal;
+            iterationStr = iterationStr.split('.')[0] + '.' + iterationDecimal;
             return Number(iterationStr);
         } else {
             return 0;
@@ -316,75 +316,75 @@ Ext.define('CustomApp', {
                 showPagingToolbar: false,
                 columnCfgs: [
                 {
-                    text: "Predecessor ID", dataIndex: "Predecessor", tdCls: "grey-background", width: 75, align: "center",
+                    text: 'Predecessor ID', dataIndex: 'Predecessor', tdCls: 'grey-background', width: 75, align: 'center',
                     getSortParam: function() {
-                      return "PredNumericID";  
+                      return 'PredNumericID';  
                     },
                     renderer: function(value, meta) {
-                        meta.tdCls = "grey-background";
+                        meta.tdCls = 'grey-background';
                         return value.FormattedID ? '<a href="' + Rally.nav.Manager.getDetailUrl(value) + '" target="_blank">' + value.FormattedID + "</a>" : void 0;
                     }
                 }, {
-                    text: "Predecessor Name", dataIndex: "PredName",  tdCls: "grey-background", width: 175
+                    text: 'Predecessor Name', dataIndex: 'PredName',  tdCls: 'grey-background', width: 175
                 }, {
-                    text: "Predecessor Project", dataIndex: "PredProject",  tdCls: "grey-background"
+                    text: 'Predecessor Project', dataIndex: 'PredProject',  tdCls: 'grey-background'
                 }, {
-                    text: "Predecessor State", dataIndex: "PredScheduleState",  tdCls: "grey-background", width: 75
+                    text: 'Predecessor State', dataIndex: 'PredScheduleState',  tdCls: 'grey-background', width: 75
                 }, {
-                    text: "Predecessor Iteration", dataIndex: "PredIteration",  tdCls: "grey-background", width: 70,
+                    text: 'Predecessor Iteration', dataIndex: 'PredIteration',  tdCls: 'grey-background', width: 70,
                     getSortParam: function() {
-                	    return "PredIterationSortNum";
+                	    return 'PredIterationSortNum';
                     }
                 }, {
-                    text: "Predecessor Due Date", dataIndex: "PredDueDate",  tdCls: "grey-background", xtype: 'datecolumn', format: 'D n/j/Y', width: 75
+                    text: 'Predecessor Due Date', dataIndex: 'PredDueDate',  tdCls: 'grey-background', xtype: 'datecolumn', format: 'D n/j/Y', width: 75
                 }, { 
-                	text: "Story ID", dataIndex: "FormattedID", xtype: "templatecolumn", width: 75, tpl: Ext.create("Rally.ui.renderer.template.FormattedIDTemplate"),
+                	text: 'Story ID', dataIndex: 'FormattedID', xtype: 'templatecolumn', width: 75, tpl: Ext.create('Rally.ui.renderer.template.FormattedIDTemplate'),
                 	getSortParam: function() {
-                	    return "StoryNumericID";
+                	    return 'StoryNumericID';
                     }
                 }, { 
-                	text: "Story Name", dataIndex: "Name", width: 175,
+                	text: 'Story Name', dataIndex: 'Name', width: 175,
                 }, { 
-                	text: "Story Project", dataIndex: "Project"
+                	text: 'Story Project', dataIndex: 'Project'
                 }, {
-                    text: "Story State", dataIndex: "StoryScheduleState", width: 75,
+                    text: 'Story State', dataIndex: 'StoryScheduleState', width: 75,
                 }, {
-                    text: "Release", dataIndex: "Release",
+                    text: 'Release', dataIndex: 'Release',
                 }, {
-                    text: "Story Iteration", dataIndex: "Iteration", width: 70,
+                    text: 'Story Iteration', dataIndex: 'Iteration', width: 70,
                     getSortParam: function() {
-                	    return "IterationSortNumber";
+                	    return 'IterationSortNumber';
                     }
                 }, {
-                    text: "Story Due Date", dataIndex: "DueDate", xtype: 'datecolumn', format: 'D n/j/Y', width: 75
+                    text: 'Story Due Date', dataIndex: 'DueDate', xtype: 'datecolumn', format: 'D n/j/Y', width: 75
                 }, {
-                    text: "Feature ID", dataIndex: "Feature", width: 65, align: "center",
+                    text: 'Feature ID', dataIndex: 'Feature', width: 65, align: 'center',
                     getSortParam: function() {
-                        return "FeatureNumericID";  
+                        return 'FeatureNumericID';  
                     },
                     renderer: function(value) {
                         return value ? '<a href="' + Rally.nav.Manager.getDetailUrl(value) + '" target="_blank">' + value.FormattedID + "</a>" : void 0;
                     }
                 }, {
-                    text: "Feature Name", dataIndex: "FeatureName", width: 175
+                    text: 'Feature Name', dataIndex: 'FeatureName', width: 175
                 }, {
-                    text: "Successor ID", dataIndex: "Successor",  tdCls: "grey-background", width: 75, align: "center",
+                    text: 'Successor ID', dataIndex: 'Successor',  tdCls: 'grey-background', width: 75, align: 'center',
                     getSortParam: function() {
-                      return "SuccNumericID";  
+                      return 'SuccNumericID';  
                     },
                     renderer: function(value) {
                         return value.FormattedID ? '<a href="' + Rally.nav.Manager.getDetailUrl(value) + '" target="_blank">' + value.FormattedID + "</a>" : void 0;
                     }
                 }, {
-                    text: "Successor Name", dataIndex: "SuccName",  tdCls: "grey-background", width: 175
+                    text: 'Successor Name', dataIndex: 'SuccName',  tdCls: 'grey-background', width: 175
                 }, {
-                    text: "Successor Project", dataIndex: "SuccProject",  tdCls: "grey-background"
+                    text: 'Successor Project', dataIndex: 'SuccProject',  tdCls: 'grey-background'
                 }, {
-                    text: "Successor State", dataIndex: "SuccScheduleState",  tdCls: "grey-background", width: 75
+                    text: 'Successor State', dataIndex: 'SuccScheduleState',  tdCls: 'grey-background', width: 75
                 }, {
-                    text: "Successor Iteration", dataIndex: "SuccIteration",  tdCls: "grey-background", width: 70,
+                    text: 'Successor Iteration', dataIndex: 'SuccIteration',  tdCls: 'grey-background', width: 70,
                     getSortParam: function() {
-                	    return "SuccIterationSortNum";
+                	    return 'SuccIterationSortNum';
                     }
                 }]
             });
@@ -421,11 +421,11 @@ Ext.define('CustomApp', {
                 var fieldName = col.dataIndex;
                 if ((fieldName === 'Predecessor' || fieldName === 'Successor' || fieldName === 'Feature') && !!record[fieldName]) {
                     text = record[fieldName].FormattedID;
-                } else if (fieldName.indexOf("DueDate") !== -1 && !!record[fieldName]) {
-                    if(typeof record[fieldName] === "string") {
-                        text = record[fieldName].split("T")[0];
+                } else if (fieldName.indexOf('DueDate') !== -1 && !!record[fieldName]) {
+                    if(typeof record[fieldName] === 'string') {
+                        text = record[fieldName].split('T')[0];
                     } else {
-                        text = Ext.Date.format(record[fieldName], "Y-m-d");
+                        text = Ext.Date.format(record[fieldName], 'Y-m-d');
                     }
                 } else {
                     text = record[fieldName];
